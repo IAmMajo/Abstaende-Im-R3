dd532ef7aa8cf9f7060b04ebbc7a938c26ee5b75
SetsunaiCodes <Nicojin.M0SIC@hotmail.com>
2023-06-15 08:38:37 +0200

    created getInput Function

---
+++ src/scripts/readInputs.js
@@ -0,0 +1,15 @@
+//Input Line
+
+import { Vector } from "./classes";
+
+//Richtungsvektoren
+let lineDirX = document.getElementById("lineDirX");
+let lineDirY = document.getElementById("lineDirY");
+let lineDirZ = document.getElementById("lineDIrZ");
+
+function getInput(){
+//Richtungsvektor
+new Vector(lineDirX.value, lineDirY.value, lineDirZ.value);
+
+
+}
