2fc419b46737efe6340f7e1282b5293c49141dc4
Majo <git@maxoverlack.dev>
2023-06-18 23:21:19 +0200

    Add design switching functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import lit from "@astrojs/lit";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [lit()],
+  output: "server",
+  adapter: cloudflare(),
 });
