f0aa9b460db14022eb79f22acae979c16d61bcf9
Majo <git@maxoverlack.dev>
2023-06-18 17:10:41 +0200

    Continue working on sketch
    Co-authored-by: Nico Puelacher <SetsunaiCodes@users.noreply.github.com>

--- src/scripts/sketch.ts
+++ src/scripts/sketch.ts
@@ -36,8 +36,8 @@ export default async (...geometrics: (Vector | Line | Plane)[]) => {
       p5.background(htmlStyle.getPropertyValue("--md-sys-color-surface"));
       geometrics.forEach((geometric) => {
         if (geometric instanceof Vector) {
-          p5.translate(geometric.x, geometric.y, geometric.z);
           p5.push();
+          p5.translate(geometric.x, geometric.y, geometric.z);
           p5.noStroke();
           p5.fill(0, 0, 255);
           p5.sphere(10);
@@ -45,28 +45,67 @@ export default async (...geometrics: (Vector | Line | Plane)[]) => {
           return;
         }
         if (geometric instanceof Line) {
-          const { base, direction: geometricDirection } = geometric;
-          const start = new P5VectorClass!(base.x, base.y, base.z);
+          const { base: geometricBase, direction: geometricDirection } =
+            geometric;
+          const p5Base = new P5VectorClass!(
+            geometricBase.x,
+            geometricBase.y,
+            geometricBase.z
+          );
           const p5Direction = new P5VectorClass!(
             geometricDirection.x,
             geometricDirection.y,
             geometricDirection.z
           );
 
-          const normalizedDirection = p5Direction.normalize();
-          const lineLength = 200;
-          const end = start.add(normalizedDirection.mult(lineLength));
+          const normalizedDirection = p5Direction.copy().normalize();
+          const lineLength = 1201;
+          const start = p5Base
+            .copy()
+            .add(normalizedDirection.copy().mult(-lineLength));
+          const end = p5Base
+            .copy()
+            .add(normalizedDirection.copy().mult(lineLength));
 
           p5.push();
           p5.noFill();
           p5.stroke(255, 0, 0);
           p5.strokeWeight(10);
+          console.log(start.x, start.y, start.z, end.x, end.y, end.z);
           p5.line(start.x, start.y, start.z, end.x, end.y, end.z);
           p5.pop();
 
           return;
         }
-        // Draw plane
+        const {
+          base: geometricBase,
+          direction1: geometricDirection1,
+          direction2: geometricDirection2,
+        } = geometric;
+        const p5Base = new P5VectorClass!(
+          geometricBase.x,
+          geometricBase.y,
+          geometricBase.z
+        );
+        const p5Direction1 = new P5VectorClass!(
+          geometricDirection1.x,
+          geometricDirection1.y,
+          geometricDirection1.z
+        );
+        const p5Direction2 = new P5VectorClass!(
+          geometricDirection2.x,
+          geometricDirection2.y,
+          geometricDirection2.z
+        );
+
+        p5.push();
+        p5.translate(p5Base.x, p5Base.y, p5Base.z);
+        p5.noStroke();
+        p5.fill(0, 255, 0, 80);
+        p5.plane(1201, 1201);
+        p5.pop();
+
+        return;
       });
     };
   }, sketchElement);
