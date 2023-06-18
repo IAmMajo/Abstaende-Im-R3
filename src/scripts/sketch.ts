2037d42cc84a722e6dc666bd41f8d9301798a1ce
Majo <git@maxoverlack.dev>
2023-06-18 14:38:31 +0200

    Only load p5 after clicking on calculate distance button

--- src/scripts/sketch.ts
+++ src/scripts/sketch.ts
@@ -1,25 +1,38 @@
-import p5, { Vector as P5Vector } from "p5";
+import "@material/web/circularprogress/circular-progress";
+import type p5 from "p5";
+import type { Vector as P5Vector } from "p5";
 import Line from "../classes/Line";
 import type Plane from "../classes/Plane";
 import Vector from "../classes/Vector";
 
 const main = document.querySelector("main")!;
 const htmlStyle = getComputedStyle(document.documentElement);
+let p5Class: typeof p5 | null = null;
+let P5VectorClass: typeof P5Vector | null = null;
 let sketch: p5 | null = null;
 
-export default (...geometrics: (Vector | Line | Plane)[]) => {
-  if (sketch) {
-    sketch.remove();
-  }
+export default async (...geometrics: (Vector | Line | Plane)[]) => {
   const sketchElement = document.createElement("div");
   sketchElement.id = "sketch";
   main.appendChild(sketchElement);
-  sketch = new p5((p5: p5) => {
+  if (p5Class && P5VectorClass) {
+    sketch?.remove();
+  } else {
+    const progress = document.createElement("md-circular-progress");
+    progress.indeterminate = true;
+    sketchElement.appendChild(progress);
+    p5Class = (await import("p5")).default;
+    P5VectorClass = p5Class.Vector;
+    if (!sketchElement.parentElement) {
+      return;
+    }
+    progress.remove();
+  }
+  sketch = new p5Class((p5: p5) => {
     p5.setup = () => {
       p5.createCanvas(1200, 800, p5.WEBGL);
     };
     p5.draw = () => {
-      console.log("craw");
       p5.background(htmlStyle.getPropertyValue("--md-sys-color-surface"));
       geometrics.forEach((geometric) => {
         if (geometric instanceof Vector) {
@@ -33,8 +46,8 @@ export default (...geometrics: (Vector | Line | Plane)[]) => {
         }
         if (geometric instanceof Line) {
           const { base, direction: geometricDirection } = geometric;
-          const start = new P5Vector(base.x, base.y, base.z);
-          const p5Direction = new P5Vector(
+          const start = new P5VectorClass!(base.x, base.y, base.z);
+          const p5Direction = new P5VectorClass!(
             geometricDirection.x,
             geometricDirection.y,
             geometricDirection.z
