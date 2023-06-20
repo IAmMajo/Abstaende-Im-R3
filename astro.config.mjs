46ff140ffb42effbd4ba3912ff809a9a2ea100b4
SetsunaiCodes <Nicojin.M0SIC@hotmail.com>
2023-06-20 15:34:30 +0200

    Added Logo and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,19 @@ import { defineConfig } from "astro/config";
 
 import lit from "@astrojs/lit";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [lit()],
+  head: {
+    meta: [
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/static/favicon.ico",
+      },
+    ],
+  },
 });
